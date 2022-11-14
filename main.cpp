8401c79b10b22c04fb03f32b77c1d90f02c4e1be
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2022-11-14 09:20:10 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,44 +1,30 @@
 #include <stdio.h>
 
-template <typename Type>
-
-Type Min(Type a, Type b)
-{
-	if (a < b)
-	{
-		return static_cast<Type>(a);
-	}
-
-	return static_cast<Type>(b);
-
-}
-
-template<>
-
-char Min<char>(char a, char b)
-{
-	printf("”š‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ\n");
-	return '\0';
-}
+int Wages(int time);
 
 int main()
 {
-	int a1 = 0;
-	int b1 = 1;
+	int time = 8;
+	int wage = 0;
+	for (int i = 0; i < time; i++)
+	{
+		wage += Wages(i);
+	}
 
-	float a2 = 2;
-	float b2 = 1;
+	printf("%dŠÔ‚Åˆê”Ê’À‹à‚Í%d‰~AÄ‹N’À‹à‚Í%d‰~", time, 1072 * time, wage);
 
-	double a3 = 3;
-	double b3 = 2;
-
-	char a4 = 'a';
-	char b4 = 'b';
-
-	printf("%d\n", Min<int>(a1, b1));
-	printf("%f\n", Min<float>(a2, b2));
-	printf("%lf\n", Min<double>(a3, b3));
-	printf("%c\n", Min<char>(a4, b4));
 	return 0;
 }
 
+int Wages(int time)
+{
+	if (time == 0)
+	{
+		return 100;
+	}
+	else
+	{
+		return Wages(time - 1) * 2 - 50;
+	}
+
+}
