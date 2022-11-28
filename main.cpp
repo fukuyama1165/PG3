e679681362a0593c4f429681bb6ee6c3d7279efd
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2022-11-28 14:44:32 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,12 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct cell
+{
 
+	int val;
+	struct cell* next;
+
+}CELL;
+
+void create(CELL* lead,int val);
+void index(CELL* lead);
 
 int main()
 {
 
+	int val;
+	CELL lead;
+	lead.next = nullptr;
+
+	while (true)
+	{
+
+		scanf_s("%d", &val);
+
+		create(&lead, val);
+
+		index(&lead);
+
+	}
 
 	return 0;
 
+
+}
+
+void create(CELL* lead, int val)
+{
+	CELL* newCell;
+
+	newCell = (CELL*)malloc(sizeof(CELL));
+
+	newCell->val = val;
+	newCell->next = nullptr;
+
+	while (lead->next != nullptr)
+	{
+
+		lead = lead->next;
+
+	}
+
+	if (newCell != nullptr)
+	{
+		lead->next = newCell;
+	}
+}
+
+void index(CELL* lead)
+{
+
+	while (lead->next != nullptr)
+	{
+
+		lead = lead->next;
+
+		printf("%d\n", lead->val);
+
+	}
 
 }
